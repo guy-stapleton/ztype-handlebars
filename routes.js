1d433bd5256c81bf0b3ed7416bc5aef7d2b0233c
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-13 08:15:04 +1300

    Set up routes to add a score to database

--- routes.js
+++ routes.js
@@ -15,12 +15,45 @@ routes.get('/add', (req, res) => {
   res.render('add', {})
 })
 
+routes.post('/add', (req, res) => {
+  var db = req.app.get('db')
+  var formDetails = req.body
+
+  db('scores')
+    .insert({date: formDetails.date,
+            score: formDetails.score,
+            typing_accuracy: formDetails.typing_accuracy,
+            wave: formDetails.wave,
+            longest_streak: formDetails.longest_streak
+          })
+    .then(result => {
+      res.redirect('/add/score-added')
+    })
+    .catch(err => {
+      res.send(`Yikes an error has struck ${err.message}`)
+    })
+})
+
+routes.get('/add/score-added', (req, res) => {
+  res.render('score-added')
+})
+
 routes.get('/recent-scores', (req, res) => {
   res.render('recent-scores')
 })
 
 routes.get('/highscores', (req, res) => {
-  res.render('highscores', {})
+  // var db = req.app.get('db')
+  //
+  // db('scores')
+  //   .select('scores')
+  //   .then(result => {
+      res.render('highscores', result)
+    // })
+    // .catch(err => {
+    //   res.send('Yikes an error ${err.message}')
+    // })
+
 })
 
 
