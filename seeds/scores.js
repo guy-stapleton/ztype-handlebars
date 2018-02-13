ca0afa7cfa6c49962a7a31e40b97c0c120c841a1
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-14 03:36:14 +1300

    Updates to seeds to remove decimnals

--- seeds/scores.js
+++ seeds/scores.js
@@ -4,9 +4,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('scores').insert([
-        {id: 1, date: '2018-02-7', score: 232, typing_accuracy: 82.1, wave: 007, longest_streak: 32},
-        {id: 2, date: '2018-02-8', score: 267, typing_accuracy: 77.7, wave: 008, longest_streak: 27},
-        {id: 3, date: '2018-02-9', score: 274, typing_accuracy: 81, wave: 007, longest_streak: 54}
+        {id: 1, date: '2018-02-7', score: 232, typing_accuracy: 82, wave: 007, longest_streak: 32},
+        {id: 2, date: '2018-02-8', score: 267, typing_accuracy: 77, wave: 008, longest_streak: 27},
+        {id: 3, date: '2018-02-9', score: 274, typing_accuracy: 81, wave: 007, longest_streak: 54},
+        {id: 4, date: '2018-02-9', score: 246, typing_accuracy: 86, wave: 006, longest_streak: 53},
+        {id: 5, date: '2018-02-9', score: 382, typing_accuracy: 82, wave: 0011, longest_streak: 36}
       ]);
     });
 };
